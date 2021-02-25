9c03626b1d80df3850efdbed3df5b22caeaafefa
garethiv <gareth@garethveale.dev>
2021-02-25 15:44:52 +0000

    index layout changes

---
+++ src/components/qanda.js
@@ -0,0 +1,19 @@
+/**
+ * Sidebar component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react"
+
+const QAndA = () => {
+
+  return (
+    <div className="qanda">
+        <p>WHAT?</p>
+    </div>
+  )
+}
+
+export default QAndA
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
+import Sidebar from "../components/sidebar"
+import QAndA from "../components/qanda"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -9,56 +10,57 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+      
+      {/* 
+        Insert side bar here
+      */}
+      <div class='index-wrapper'>
+        <div class='index-column-l'>
+          <div class='left-column'>
+            <Sidebar />
+          </div>
+        </div>
+        <div class='index-column-r'>
+          <div class='right-column'>
+            <QAndA />
+            <ol style={{ listStyle: `none` }}>
+              {posts.map(post => {
+                const title = post.frontmatter.title || post.fields.slug
 
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
+                return (
+                  <li key={post.fields.slug}>
+                    <article
+                      className="post-list-item"
+                      itemScope
+                      itemType="http://schema.org/Article"
+                    >
+                      <header>
+                        <h2>
+                          <Link to={post.fields.slug} itemProp="url">
+                            <span itemProp="headline">{title}</span>
+                          </Link>
+                        </h2>
+                        <small>{post.frontmatter.date}</small>
+                      </header>
+                      <section>
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html: post.frontmatter.description || post.excerpt,
+                          }}
+                          itemProp="description"
+                        />
+                      </section>
+                    </article>
+                  </li>
+                )
+              })}
+            </ol>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
